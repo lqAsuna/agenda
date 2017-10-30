db53596de416a4164a3729d4da663aff84156f65
lqAsuna <32841893+lqAsuna@users.noreply.github.com>
2017-10-30 21:41:33 +0800

    Update register.go

--- src/agenda/cmd/register.go
+++ src/agenda/cmd/register.go
@@ -15,39 +15,38 @@
 package cmd
 
 import (
+	"agenda/entity"
 	"fmt"
 
 	"github.com/spf13/cobra"
 )
 
-// registerCmd represents the register command
-var registerCmd = &cobra.Command{
-	Use:   "register",
+var registerCmd = &cobra.Command {
+	Use:   "qm",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
-
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		username, _ := cmd.Flags().GetStringArray("user")
-		fmt.Printf("register called by %v", username)
+		name, _ := cmd.Flags().GetString("name")
+		password, _ := cmd.Flags().GetString("password")
+		email, _ := cmd.Flags().GetString("email")
+		phone, _ := cmd.Flags().GetString("phone")
+		ur := entity.GetUser(name, password, email, phone)
+		if AgendaS.UserRegister(ur) == true {
+			fmt.Printf("Regist Succeed!")
+		} else {
+			fmt.Printf("Regist Failed!")
+		}
 	},
 }
 
 func init() {
-	RootCmd.AddCommand(registerCmd)
-
-	registerCmd.Flags().StringArrayP("user", "u", nil, "us")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// registerCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// registerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	RootCmd.AddCommand(userloginCmd)
+	registerCmd.Flag().StringP("name", "n", "Anonymous", "提供用户名字")
+	registerCmd.Flag().StringP("password", "p", "Anonymous", "提供用户密码")
+	registerCmd.Flag().StringP("email", "e", "Anonymous", "提供用户邮箱地址")
+	registerCmd.Flag().StringP("phone", "o", "Anonymous", "提供用户手机号码")
 }
