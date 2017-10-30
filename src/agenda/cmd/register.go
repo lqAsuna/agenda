dab0514f07d74e8c0141869ad9704876b984c474
lqAsuna <32841893+lqAsuna@users.noreply.github.com>
2017-10-30 21:42:56 +0800

    Update register.go

--- src/agenda/cmd/register.go
+++ src/agenda/cmd/register.go
@@ -49,4 +49,13 @@ func init() {
 	registerCmd.Flag().StringP("password", "p", "Anonymous", "提供用户密码")
 	registerCmd.Flag().StringP("email", "e", "Anonymous", "提供用户邮箱地址")
 	registerCmd.Flag().StringP("phone", "o", "Anonymous", "提供用户手机号码")
+		// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// dmCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// dmCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
